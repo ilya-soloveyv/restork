c3e85f6c357b778266a8da0bc002588821b43b06
Ilya <ilya.soloveyv@gmail.com>
2019-12-03 19:14:10 +0300

    fix

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="restork">
+    Restork
+    <nuxt-link to="/admin">administrator</nuxt-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#restork {
+  background: #ddd;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: bold;
+  text-transform: uppercase;
+  a {
+    font-weight: normal;
+    font-size: 10px;
+  }
+}
+</style>
