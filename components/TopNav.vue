4c15a26100809486de7785a3cd55acfff47f92ac
ilya <ilya.soloveyv@gmail.com>
2020-05-24 02:05:39 +0300

    fix

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,46 +1,57 @@
 <template>
-  <b-navbar toggleable="lg">
-    <b-container>
-      <b-navbar-brand to="/">
-        <img src="~assets/images/logo.svg" alt="" />
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/">Подобрать жилье</b-nav-item>
-          <b-nav-item to="/">Как это работает</b-nav-item>
-          <b-nav-item to="/">О сервисе</b-nav-item>
-          <b-nav-item to="/">Подобрать жилье</b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-          <template v-if="$auth.$state.loggedIn">
-            <div class="restorkUser">
-              <template v-if="$auth.user.sUserAvatar">
-                <img
-                  :src="
-                    SELECTEL_WEB + '/user/preview/' + $auth.user.sUserAvatar
-                  "
-                />
-              </template>
-              <template v-else>
-                <div class="empty">
-                  <img src="~/static/user.svg" />
-                </div>
-              </template>
-            </div>
-            <b-nav-item to="/dashboard">Личный кабинет</b-nav-item>
-            <b-nav-item @click.prevent="$auth.logout()" to="/">
-              Выйти
-            </b-nav-item>
-          </template>
-          <template v-else>
-            <b-nav-item to="/signin">Войти</b-nav-item>
-            <b-nav-item to="/signup">Регистрация</b-nav-item>
-          </template>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-container>
-  </b-navbar>
+  <div>
+    <b-navbar toggleable="lg">
+      <b-container>
+        <b-navbar-brand to="/">
+          <img src="~assets/images/logo.svg" alt="" />
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Подобрать жилье</b-nav-item>
+            <b-nav-item to="/">Как это работает</b-nav-item>
+            <b-nav-item v-b-toggle.sidebar-1>О сервисе</b-nav-item>
+            <b-nav-item to="/">Подобрать жилье</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <template v-if="$auth.$state.loggedIn">
+              <div class="restorkUser">
+                <template v-if="$auth.user.sUserAvatar">
+                  <img
+                    :src="
+                      SELECTEL_WEB + '/user/preview/' + $auth.user.sUserAvatar
+                    "
+                  />
+                </template>
+                <template v-else>
+                  <div class="empty">
+                    <img src="~/static/user.svg" />
+                  </div>
+                </template>
+              </div>
+              <b-nav-item to="/dashboard">Личный кабинет</b-nav-item>
+              <b-nav-item @click.prevent="$auth.logout()" to="/">
+                Выйти
+              </b-nav-item>
+            </template>
+            <template v-else>
+              <b-nav-item to="/signin">Войти</b-nav-item>
+              <b-nav-item to="/signup">Регистрация</b-nav-item>
+            </template>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <b-sidebar id="sidebar-1" title="Sidebar" shadow backdrop no-slide>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras mattis
+          consectetur purus sit amet fermentum. consectetur purus sit amet
+          fermentum.
+        </p>
+      </div>
+    </b-sidebar>
+  </div>
 </template>
 
 <script>
