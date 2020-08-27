c6e0111eaffb8012bb8182048c50cb97a1de4b3a
ilya <ilya.soloveyv@gmail.com>
2020-08-28 02:05:58 +0300

    fix

--- pages/dashboard/object/edit/_iObjectID.js
+++ pages/dashboard/object/edit/_iObjectID.js
@@ -13,11 +13,36 @@ export default {
   computed: {
     object({ $store }) {
       return $store.state.object.item
+    },
+    objectOptions({ $store }) {
+      return $store.state.objectOption.list
+    },
+    objectObjectOptionsArray: {
+      get() {
+        // this.$set(this, 'loadingDisabledCheckbox', false)
+        return this.$store.state.object.item.object_object_options_array
+      },
+      set(objectObjectOptionsArray) {
+        console.log(objectObjectOptionsArray)
+      }
+    },
+    roomOptions({ $store }) {
+      return $store.state.roomOption.list
+    },
+    objectRoomOptionsArray: {
+      get() {
+        return this.$store.state.object.item.object_room_options_array
+      },
+      set(objectRoomOptionsArray) {
+        console.log(objectRoomOptionsArray)
+      }
     }
   },
   async asyncData({ store, $axios, params }) {
     await store.dispatch('object/GET_ITEM', {
       iObjectID: params.iObjectID
     })
+    await store.dispatch('objectOption/GET_LIST')
+    await store.dispatch('roomOption/GET_LIST')
   }
 }
--- pages/dashboard/object/edit/_iObjectID.vue
+++ pages/dashboard/object/edit/_iObjectID.vue
@@ -14,7 +14,7 @@
           label-for="input-sObjectTitle"
         >
           <b-form-input
-            id="input-1"
+            id="input-sObjectTitle"
             v-model="object.sObjectTitle"
             type="text"
             required
@@ -23,37 +23,38 @@
       </div>
       <div class="form-group-wrap form-group-wrap-line2">
         <b-form-group
-          id="label-sObjectTitle"
+          id="label-sObjectTypeTitle"
           label="Категорию жилья"
-          label-for="input-sObjectTitle"
+          label-for="input-sObjectTypeTitle"
         >
           <b-form-input
-            id="input-1"
-            v-model="object.sObjectTitle"
+            id="input-sObjectTypeTitle"
+            v-model="object.object_type.sObjectTypeTitle"
+            disabled
             type="text"
             required
           ></b-form-input>
         </b-form-group>
         <b-form-group
-          id="label-sObjectTitle"
+          id="label-iObjectRoomCount"
           label="Комнат"
-          label-for="input-sObjectTitle"
+          label-for="input-iObjectRoomCount"
         >
           <b-form-input
-            id="input-1"
-            v-model="object.sObjectTitle"
+            id="input-iObjectRoomCount"
+            v-model="object.iObjectRoomCount"
             type="text"
             required
           ></b-form-input>
         </b-form-group>
         <b-form-group
-          id="label-sObjectTitle"
+          id="label-iObjectBed"
           label="Спальных мест"
-          label-for="input-sObjectTitle"
+          label-for="input-iObjectBed"
         >
           <b-form-input
-            id="input-1"
-            v-model="object.sObjectTitle"
+            id="input-iObjectBed"
+            v-model="object.iObjectBed"
             type="text"
             required
           ></b-form-input>
@@ -61,49 +62,49 @@
       </div>
       <div class="form-group-wrap form-group-wrap-line3">
         <b-form-group
-          id="label-sObjectTitle"
+          id="label-iObjectPlace"
           label="Площадь участка"
-          label-for="input-sObjectTitle"
+          label-for="input-iObjectPlace"
         >
           <b-form-input
-            id="input-1"
-            v-model="object.sObjectTitle"
+            id="input-iObjectPlace"
+            v-model="object.iObjectPlace"
             type="text"
             required
           ></b-form-input>
         </b-form-group>
         <b-form-group
-          id="label-sObjectTitle"
+          id="label-iObjectPlace"
           label="Площадь дома"
-          label-for="input-sObjectTitle"
+          label-for="input-iObjectPlace"
         >
           <b-form-input
-            id="input-1"
-            v-model="object.sObjectTitle"
+            id="input-iObjectPlace"
+            v-model="object.iObjectPlace"
             type="text"
             required
           ></b-form-input>
         </b-form-group>
         <b-form-group
-          id="label-sObjectTitle"
+          id="label-iObjectPlaceDop"
           label="Этаж"
-          label-for="input-sObjectTitle"
+          label-for="input-iObjectPlaceDop"
         >
           <b-form-input
-            id="input-1"
-            v-model="object.sObjectTitle"
+            id="input-iObjectPlaceDop"
+            v-model="object.iObjectPlaceDop"
             type="text"
             required
           ></b-form-input>
         </b-form-group>
         <b-form-group
-          id="label-sObjectTitle"
+          id="label-iObjectPlaceDop"
           label="Этажность"
-          label-for="input-sObjectTitle"
+          label-for="input-iObjectPlaceDop"
         >
           <b-form-input
-            id="input-1"
-            v-model="object.sObjectTitle"
+            id="input-iObjectPlaceDop"
+            v-model="object.iObjectPlaceDop"
             type="text"
             required
           ></b-form-input>
@@ -111,24 +112,81 @@
       </div>
       <div class="form-group-wrap form-group-wrap-line1">
         <b-form-group
-          id="label-sObjectTitle"
-          label="Название объекта"
-          label-for="input-sObjectTitle"
+          id="label-tObjectDesc"
+          label="Описание"
+          label-for="input-tObjectDesc"
         >
           <b-form-textarea
-            id="input-1"
-            v-model="object.sObjectTitle"
+            id="input-tObjectDesc"
+            v-model="object.tObjectDesc"
             rows="5"
             max-rows="10"
           ></b-form-textarea>
         </b-form-group>
       </div>
-      <p v-for="i in 10" :key="i">
+      <h2>Удобства отеля</h2>
+      <b-form-checkbox-group v-model="objectObjectOptionsArray">
+        <ul class="optionsCheckboxList">
+          <li v-for="(option, index) in objectOptions" :key="index">
+            <b-form-checkbox :value="option.iObjectOptionID">
+              {{ option.sObjectOptionTitle }}
+            </b-form-checkbox>
+          </li>
+        </ul>
+      </b-form-checkbox-group>
+      <h2>Удобства комнат</h2>
+      <b-form-checkbox-group v-model="objectObjectOptionsArray">
+        <ul class="optionsCheckboxList">
+          <li v-for="(option, index) in roomOptions" :key="index">
+            <b-form-checkbox :value="option.iRoomOptionID">
+              {{ option.sRoomOptionTitle }}
+            </b-form-checkbox>
+          </li>
+        </ul>
+      </b-form-checkbox-group>
+      <h2>Местоположение</h2>
+      <div class="form-group-wrap form-group-wrap-line1">
+        <b-form-group
+          id="label-sObjectAddress"
+          label="Адрес"
+          label-for="input-sObjectAddress"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="object.sObjectAddress"
+            type="text"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </div>
+      <div id="map">
+        <client-only>
+          <yandex-map
+            :coords="object.aObjectCoordinate.coordinates"
+            :controls="[]"
+            :zoom="14"
+            :draggable="true"
+          >
+            <ymap-marker
+              :coords="object.aObjectCoordinate.coordinates"
+              marker-id="123"
+              hint-content="Расположение объекта"
+            />
+          </yandex-map>
+        </client-only>
+      </div>
+      <h2>Основные места рядом</h2>
+      <!-- <pre>{{ objectOptions }}</pre>
+      <pre>{{ roomOptions }}</pre>
+      <pre>{{ objectObjectOptionsArray }}</pre>
+      <pre>{{ objectRoomOptionsArray }}</pre> -->
+      <pre>{{ object }}</pre>
+      <!-- <p v-for="i in 10" :key="i">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
         quos accusamus! Nostrum omnis autem exercitationem! Incidunt
         perspiciatis fugiat error pariatur nihil nemo adipisci consectetur eaque
         voluptate, placeat dolor possimus obcaecati.
-      </p>
+      </p> -->
       <!-- <pre>{{ object }}</pre> -->
     </div>
     <div class="wrapStatusTop">
