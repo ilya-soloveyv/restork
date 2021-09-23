33cefd399b855be7f4b35f63618e32232eadd02b
Ilya <ilya.soloveyv@gmail.com>
2021-09-23 17:47:50 +0300

    fix

--- pages/tutorial/step/photo.vue
+++ pages/tutorial/step/photo.vue
@@ -91,7 +91,16 @@
       </dnd-zone> -->
       <div class="upload">
         <div v-show="dropzoneLoading" class="upload__loading">
-          Ожидайте загрузки...
+          <svg class="upload__spinner" viewBox="0 0 50 50">
+            <circle
+              class="path"
+              cx="25"
+              cy="25"
+              r="20"
+              fill="none"
+              stroke-width="5"
+            />
+          </svg>
         </div>
         <dropzone
           id="objectDropzone"
@@ -263,8 +272,46 @@ export default {
   &__loading,
   ::v-deep .dropzone {
     width: 100%;
+    height: 114px;
     padding: 32px 0;
     text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__spinner {
+    animation: rotate 2s linear infinite;
+    z-index: 2;
+    width: 50px;
+    height: 50px;
+
+    & .path {
+      stroke: hsl(210, 70, 75);
+      stroke-linecap: round;
+      animation: dash 1.5s ease-in-out infinite;
+    }
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
   }
 }
 </style>
