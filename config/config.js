d5c803b65c3f7e3a4b57df6b353d9f6879514a69
Ilya Soloveyv <ilya.soloveyv@gmail.com>
2023-03-07 11:03:03 +0300

    change config db

--- config/config.js
+++ config/config.js
@@ -11,17 +11,17 @@ module.exports = {
     logging: false
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql'
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql'
   }
 }
